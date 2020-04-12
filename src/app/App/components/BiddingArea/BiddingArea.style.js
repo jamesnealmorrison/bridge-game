9d85072c260f474c375df6ae9e79828fd34682b0
Jim Morrison <jamesnealmorrison@users.noreply.github.com>
2020-04-12 13:20:58 -0500

    Specify which column/rows each area is in, rather than trusting the default.

--- src/app/App/components/BiddingArea/BiddingArea.style.js
+++ src/app/App/components/BiddingArea/BiddingArea.style.js
@@ -3,4 +3,6 @@ import styled, { css } from 'styled-components'
 export const BiddingAreaLayout = styled.div`
   font-size: 1.25rem;
   font-weight: normal;
+  grid-column: 2;
+  grid-row: 1;
 `
--- src/app/App/components/MenuArea/MenuArea.style.js
+++ src/app/App/components/MenuArea/MenuArea.style.js
@@ -3,4 +3,6 @@ import styled, { css } from 'styled-components'
 export const MenuAreaLayout = styled.div`
   font-size: 1.25rem;
   font-weight: normal;
+  grid-column: 1;
+  grid-row: 1;
 `
